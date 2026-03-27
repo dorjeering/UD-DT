7763bce3836e4b3ea427660d95e80159373a9bf0
unknown <08250267.sce@rub.edu.bt>
2026-03-27 15:53:12 +0600

    k

---
+++ pairWork.py
@@ -0,0 +1,8 @@
+print()
+n= input("Enter your name: ")
+m1=float(input("Marks of module 1"))
+m2=float(input("Marks of module 2"))
+m3=float(input("Marks of module 3"))
+m4=float(input("Marks of module 4"))
+a= float(input("Enter the number of classes attended: "))
+att=(a/96)*96
