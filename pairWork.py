d56339b5cb0f2402613a5ffa2a6f3234de92cfbe
unknown <08250267.sce@rub.edu.bt>
2026-03-27 15:59:32 +0600

    k

--- pairWork.py
+++ pairWork.py
@@ -6,3 +6,6 @@ m3=float(input("Marks of module 3"))
 m4=float(input("Marks of module 4"))
 a= float(input("Enter the number of classes attended: "))
 att=(a/96)*96
+
+if (m1 and m2 and m3 and m4)>=50:
+    print("pass")
